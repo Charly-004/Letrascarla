78c47d9b6885a6352a99d2b7a151ebc4f9542bca
jordy <marcelo2005jmsp@gmail.com>
2024-10-30 12:03:07 -0500

    Proyecto letras

---
+++ src/main/java/ec/edu/espoch/letrascarla/LetrasCarla.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.letrascarla;
+
+/**
+ *
+ * @author jordy
+ */
+public class LetrasCarla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
